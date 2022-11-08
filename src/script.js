b179aa6cfbaaf6b8054678bfb529be44cfdef700
thebladehit <boqdanyarmolka@gmail.com>
2022-11-08 22:55:32 +0200

    Add checker for chest.

--- src/script.js
+++ src/script.js
@@ -3,31 +3,26 @@
 //initialization
 function createAreaForGame() {
     const COLNUMBER = 8;
-    let area = [];
+    const area = [];
     let counterBlack = 1;
     let counterId = 0;
 
     for (let row = 0; row < COLNUMBER; row++) {
-        let res = [];
+        const res = [];
 
         for (let col = 0; col < COLNUMBER; col++, counterId++) {
             if ((col + counterBlack) % 2 === 0){
+                const element = document.createElement('div');
+                element.className = 'col black';
+                element.id = `${row}, ${col}`;
 
-                res.push({
-                    // positionX: row,
-                    // positionY: col,
-                    id: counterId,
-                    content: `<div id="${counterId}" class="black col"></div>`//${counterId}
-                });
+                res.push(element);
             }else {
+                const element = document.createElement('div');
+                element.className = 'col white';
+                element.id = `${row}, ${col}`;
 
-                res.push({
-                    // positionX: row,
-                    // positionY: col,
-                    id: counterId,
-                    content: `<div id="${counterId}" class="white col"></div>`//${counterId}
-                    
-                });
+                res.push(element);
             }
         }
 
@@ -43,106 +38,117 @@ function drawArea(area) {
     const baza = document.getElementById('baza');
 
     for (const row of area) {
-        let res = '';
+        const baza_row = document.createElement('div');
+        baza_row.id = 'row';
 
         for(const col of row) {
-            res += col.content;
+            baza_row.append(col);
         }
 
-        baza.innerHTML += `<div id="row">${res}</div>`;
+        baza.append(baza_row);
     }
 }
 
-// function createChest(id, previousElement = undefined) {
-//     if (previousElement !== undefined) {
-//         previousElement.remove();
-//     }
-//
-//     const chest = document.createElement('div');
-//     chest.className = 'chestRed';
-//     chest.id = id;
-//     chest.addEventListener('click', () => checkAvaliableCol(chest));
-//
-//     document.getElementById(`${chest.id}`).append(chest);
-// }
 
-function createChestsFirstPlayer(area) {
+function createChestsFirstPlayer() {
     const CHEST_NUMBER = 12;
-
-    let startFromElementOfRow = 1;
-    let rowNumber = 0;
     let chests = 0
-    // console.log(area);
 
-    for (const row of area) {
-        // console.log(startFromElementOfRow);
+    const arrChests = [];
 
-        let startFromElementOfCol = startFromElementOfRow;
+    let row = 0;
+    let col = 0
+    while (chests < CHEST_NUMBER) {
+        const elem = document.getElementById(`${row}, ${col}`);
 
-        for (const col of row) {
-            console.log(chests);
-            if (col.id === startFromElementOfCol + rowNumber && chests < CHEST_NUMBER) {
-                const chest = document.createElement('div');
-                chest.className = 'chest chestGreen';
-                chest.addEventListener('click', () => chest.classList.toggle('shadow'));
-                document.getElementById(col.id).append(chest);
-                startFromElementOfCol += 2;
-                chests++;
-            }
+        if (elem.className === 'col black') {
+            const chest = document.createElement('div');
+            chest.className = 'chest chestGreen';
+            chest.row = row;
+            chest.col = col;
+            chest.addEventListener('click', () => {
+                chestActive(chest, arrChests);
+                checkAvaliable(chest, arrChests);
+            });
+
+            elem.append(chest);
+            arrChests.push(chest);
+
+            chests++;
         }
-        startFromElementOfRow = (startFromElementOfRow === 1) ? 0 : 1;
-        rowNumber += 8;
+
+        col++;
+        if (col === 8) {
+            row++;
+            col = 0;
+        }
     }
 }
 
 
-function createChestsSecondPlayer(area) {
+function createChestsSecondPlayer() {
     const CHEST_NUMBER = 12;
-
-    let startFromElementOfRow = 0;
-    let rowNumber = 56;
     let chests = 0
-    // console.log(area);
 
-    for (const row of area.reverse()) {
-        // console.log(startFromElementOfRow);
+    let row = 7;
+    let col = 0
+    while (chests < CHEST_NUMBER) {
+        const elem = document.getElementById(`${row}, ${col}`);
 
-        let startFromElementOfCol = startFromElementOfRow;
+        if (elem.className === 'col black') {
+            const chest = document.createElement('div');
+            chest.className = 'chest chestRed';
+            chest.addEventListener('click', () => chest.classList.toggle('active'));
 
-        for (const col of row) {
-            console.log(chests);
-            if (col.id === startFromElementOfCol + rowNumber && chests < CHEST_NUMBER) {
-                const chest = document.createElement('div');
-                chest.className = 'chest chestRed';
-                chest.addEventListener('click', () => chest.classList.toggle('shadow'));
-                document.getElementById(col.id).append(chest);
-                startFromElementOfCol += 2;
-                chests++;
-            }
+            elem.append(chest);
+            chests++;
         }
-        startFromElementOfRow = (startFromElementOfRow === 1) ? 0 : 1;
-        rowNumber -= 8;
+
+        col++;
+        if (col === 8) {
+            row--;
+            col = 0;
+        }
     }
 }
 
 
-// function checkAvaliableCol(elem) {
-//     const FIRSTMOVE = +elem.id + 7;
-//     const SECONDMOVE = +elem.id + 9;
-//
-//     elem.classList.toggle('shadow');
-//
-//     const elementFirst = document.getElementById(FIRSTMOVE);
-//     const elementSecond = document.getElementById(SECONDMOVE);
-//
-//     elementFirst.addEventListener('click', () => createChest(FIRSTMOVE, elem));
-//
-//
-//     elementFirst.classList.toggle('avaliable');
-//     elementSecond.classList.toggle('avaliable');
-// }
+function chestActive(chest, arrChest) {
+    for (const anotherChest of arrChest) {
+        if (anotherChest !== chest) {
+            anotherChest.classList.remove('active');
+        }
+    }
+    chest.classList.toggle('active');
+}
+
+
+function checkAvaliable(chest) {
+    try {
+        // console.log(chest.row, chest.col);
+        const firstMove = document.getElementById(`${chest.row + 1}, ${chest.col - 1}`);
+        const secondMove = document.getElementById(`${chest.row + 1}, ${chest.col + 1}`);
+
+        for (const col of area.flat()) {
+            col.classList.remove('avaliable');
+        }
+
+        if (firstMove.innerHTML === '' && chest.className.includes('active')) {
+            firstMove.classList.add('avaliable');
+        }
+
+        if (secondMove.innerHTML === '' && chest.className.includes('active')) {
+            secondMove.classList.add('avaliable');
+        }
+    }
+    catch (e) {
+
+    }
+}
+
 
 const area = createAreaForGame();
+// console.log(area);
 drawArea(area);
 createChestsFirstPlayer(area);
 createChestsSecondPlayer(area);
